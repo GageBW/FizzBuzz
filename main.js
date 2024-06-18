0e474eff1ad16285218f0f82e81dbb25993972a6
GageBW <gagebw@gmail.com>
2024-06-18 14:16:09 -0700

    added comments

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 for (let num = 1; num <= 100; num++) {
+    //numbers divisable by 3 and 5 will appear as 'FizzBuzz'
     if (num % 3 === 0 && num % 5 === 0) {
         console.log("FizzBuzz")
     }
+    //numbers divisable by 3 but not 5 will appear as 'Fizz'
     else if (num % 3 === 0) {
         console.log("Fizz")
     } 
+    //numbers divisable by 5 but not 3 will appear as 'Buzz'
     else if (num % 5 === 0) {
         console.log("Buzz")
     }
+    //all other numbers will be unaltered
     else {
         console.log(num)
     }
